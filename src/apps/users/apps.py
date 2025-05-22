2cfa8ed5559774c6f0bfeaba019206cd2d0247e4
vladkleshko <vkoesko@gmail.com>
2025-05-22 15:16:54 +0300

    created app users

--- src/apps/users/apps.py
+++ src/apps/users/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class EventConfig(AppConfig):
+class UsersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'apps.events'
+    name = 'apps.users'
